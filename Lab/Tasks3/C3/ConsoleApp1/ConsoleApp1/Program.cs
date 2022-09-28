7c2d31b05ad818b97c7fd832f766f9e5492bb5e9
fatima-28 <2n6gddb@code.edu.az>
2022-09-28 14:26:16 -0700

    tasks added

---
+++ Lab/Tasks3/C3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 30,2, 28, 4, 9, 140, 3 };
+            int x = 0;
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                for (int j = 0; j < arr.Length-1; j++)
+                {
+                    if (arr[j]>arr[j+1])
+                    {
+                        x = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = x;
+                    }
+                } 
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+        }
+    }
+}
