7d735ee09fccc7c12ef93a478589b953753cb071
fatima-28 <2n6gddb@code.edu.az>
2023-04-10 05:02:31 -0700

    fetch promise

---
+++ Html Css Js/Promise.Fetch/custom.js
@@ -0,0 +1,22 @@
+[
+    {
+        "name":"Emil",
+        "age":21
+    },
+    {
+        "name":"Fat",
+        "age":20
+    },
+     {
+        "name":"Nicat",
+        "age":21
+    },
+    {
+        "name":"Vusal",
+        "age":19
+    },
+    {
+        "name":"Leman",
+        "age":21
+    }
+]
